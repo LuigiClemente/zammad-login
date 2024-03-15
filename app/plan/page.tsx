d20e0483afcad9bcb0eaf6781fd5ef37e9405fd6
unknown <ngobao2001@gmail.com>
2024-03-15 20:58:18 +0700

    Rename file

--- app/plan/page.tsx
+++ app/plan/page.tsx
@@ -4,7 +4,7 @@ export const metadata = genPageMetadata({ title: 'Plan' })
 const DynamicComponent = dynamic(() => import('@/components/CubeComponent'), {
   ssr: false, // This ensures the component is only imported on the client side
 })
-export default function Cube() {
+export default function Plan() {
   return (
     <>
       <DynamicComponent />
