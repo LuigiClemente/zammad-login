15e58a6f0032a8261b2dfc28f3ccf6603f5b82dc
unknown <ngobao2001@gmail.com>
2024-03-15 19:44:14 +0700

    Update url

--- config-apps.ts
+++ config-apps.ts
@@ -1,4 +1,4 @@
 // export const CUBE_URL = 'http://localhost:3001/#/'
 
 // export const CUBE_URL = 'https://dashboard-cube-mac7aa75j-hadisa.vercel.app/#/'
-export const CUBE_URL = 'https://front-cube.vercel.app'
+export const CUBE_URL = 'http://5.75.148.51:3001/products/family'
